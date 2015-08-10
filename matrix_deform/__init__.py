e545e93a7c6c549539ac555fbc8f24afb1675c14
Roy Nieterau <roy_nieterau@hotmail.com>
2015-08-10 07:27:24 +0200

    Add `load` method to the package to load the plug-in.

--- matrix_deform/__init__.py
+++ matrix_deform/__init__.py
@@ -1,2 +1,15 @@
 
 from .version import *
+
+
+def load():
+    """Load the plug-in from the package.
+
+    To allow Maya the read the plug-in file correctly the `matrix_deform`
+    package should be importable.
+    """
+    import os
+    import maya.cmds as mc
+    directory = os.path.dirname(__file__)
+    plugin = os.path.join(directory, 'plugin.py')
+    mc.loadPlugin(plugin, quiet=True)
